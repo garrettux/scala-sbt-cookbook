ecd0a7cb835f73df3694549ad5c4adac6b40bb49
Sean Laurent <organicveggie@gmail.com>
2012-07-26 15:45:21 -0500

    Update supported OS list.

--- metadata.rb
+++ metadata.rb
@@ -3,5 +3,7 @@ license          "All rights reserved"
 description      "Installs/Configures scala-sbt"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
-supports "ubuntu"
+%w{redhat centos scientific fedora debian ubuntu}.each do |os|
+  supports os
+end
 %w[ java apt ].each { |cb| depends cb }
