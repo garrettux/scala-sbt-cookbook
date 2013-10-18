423c9bcf4658f087f8c89cf130e41b0650c4065f
Michel Breevoort <m.breevoort@gmail.com>
2013-10-18 10:05:50 +0200

    fix for override default values chef.json in Vagrantfile" :scala_sbt => {   :repo_url => {     :redhat => 'http://scalasbt.artifactoryonline.com/scalasbt/sbt-native-packages/org/scala-sbt/sbt/0.13.0/sbt.rpm'   } }

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,2 @@
-set[:scala_sbt][:repo_url][:debian] = "http://scalasbt.artifactoryonline.com/scalasbt/sbt-native-packages/org/scala-sbt/sbt/0.12.2/sbt.deb"
-set[:scala_sbt][:repo_url][:redhat] = "http://scalasbt.artifactoryonline.com/scalasbt/sbt-native-packages/org/scala-sbt/sbt/0.12.2/sbt.rpm"
+default[:scala_sbt][:repo_url][:debian] = "http://scalasbt.artifactoryonline.com/scalasbt/sbt-native-packages/org/scala-sbt/sbt/0.12.2/sbt.deb"
+default[:scala_sbt][:repo_url][:redhat] = "http://scalasbt.artifactoryonline.com/scalasbt/sbt-native-packages/org/scala-sbt/sbt/0.12.2/sbt.rpm"
